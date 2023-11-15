83add3921fc75aecd930b3e686218651215c35fd
Renita Blessy <renibless23@gmail.com>
2023-11-15 22:14:18 +0530

    footer commit

--- ui/src/components/page3/page3.js
+++ ui/src/components/page3/page3.js
@@ -1,12 +1,33 @@
 
 
 import "./page3.scss";
+import facebook from "../../assets/facebook.png";
+import twitter from "../../assets/twitter.png";
+import instagram from "../../assets/instagram.png";
+import tiktok from "../../assets/tiktok.png";
+
 
 function Page3() {
   return (
    <div className="animaitonDiv">
     <div className="square"></div>
+    <div className="mainFooter">
+        <div className="footerPage">
+            <p className="footerHome">Home</p>
+            <p className="footerHome">page 1</p>
+            <p className="footerHome">page 2</p>
+        </div>
+        <div className="footerBar"></div>
+        <div className="footerIcons">
+            <img src={facebook}/>
+            <img src={twitter}/>
+            <img src={instagram}/>
+            <img src={tiktok}/>
+        </div>
+        <div className="copyright">© Logo, Inc.</div>
     </div>
+    </div>
+   
   );
 }
 
